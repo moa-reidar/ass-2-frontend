09b5a4ddd78ccc8f83f27aea9f69b645565dd96d
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-09 18:11:35 +0200

    fixed minor bugs on inventory.js

--- src/js/inventory.js
+++ src/js/inventory.js
@@ -1,40 +1,33 @@
-
+// Inventory.js
 import { Medicine } from './Medicine.js';
 
 export class Inventory {
     constructor() {
-        
-        this.medicines = localStorage.getItem('medicines') ? JSON.parse(localStorage.medicines) : [];
+        this.medicines = JSON.parse(localStorage.getItem('medicines')) || [];
     }
 
     addMedicine(productName, manufacturer, expirationDate, quantity) {
-       
         const newMedicine = new Medicine(productName, manufacturer, expirationDate, quantity);
         this.medicines.push(newMedicine);
         this.updateLocalStorage();
     }
 
     deleteMedicine(productId) {
-      
-        this.medicines = this.medicines.filter(medicine => medicine.productId == productId);
+        this.medicines = this.medicines.filter(medicine => medicine.productId !== productId);
         this.updateLocalStorage();
     }
 
     updateMedicine(productId, updatedData) {
-        
         const medicine = this.medicines.find(medicine => medicine.productId === productId);
         Object.assign(medicine, updatedData);
         this.updateLocalStorage();
     }
 
     updateLocalStorage() {
-       
-        localStorage.setItem('medicins', JSON.stringify(this.medicines));
+        localStorage.setItem('medicines', JSON.stringify(this.medicines));
     }
 
     getMedicines() {
-       
         return this.medicines;
     }
 }
-
