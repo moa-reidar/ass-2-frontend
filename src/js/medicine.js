c9b4f6785e71109b7ea3df10a7d2f6aaf14bbaa7
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-09 15:46:26 +0200

    Add Medicine class with unique ID generation using Date.now()

---
+++ src/js/medicine.js
@@ -0,0 +1,9 @@
+export class Medicine {
+    constructor(productName, manufacturer, expirationDate, quantity) {
+        this.productId = Date.now(); 
+        this.productName = productName;
+        this.manufacturer = manufacturer;
+        this.expirationDate = expirationDate;
+        this.quantity = quantity;
+    }
+}
