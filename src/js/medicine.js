7531a3dfef141ac2c30a95f89710cb8c1c288298
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-15 14:22:56 +0200

    tested if ProductId works

--- src/js/medicine.js
+++ src/js/medicine.js
@@ -1,9 +1,13 @@
 export class Medicine {
     constructor(productName, manufacturer, expirationDate, quantity) {
         this.productId = Date.now();
+        console.log("Laget medisin med ID:", this.productId);
         this.productName = productName;
         this.manufacturer = manufacturer;
         this.expirationDate = expirationDate;
         this.quantity = quantity;
+        
     }
 }
+
+ 
